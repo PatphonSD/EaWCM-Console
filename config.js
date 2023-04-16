0d55e45747f9b1fb1c19195e63d0de0741c664f0
PatphonSD <muc5674@gmail.com>
2023-04-17 01:33:03 +0700

    2.1.3
    เพิ่ม Billing Modal แก้ไขปัญหา matching vps  ไม่เสถียร

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const origin = "EaWCMCS/v2"; // กำหนด origin ที่ต้องการ
 const config = {
   css: ["../extension/bootstrap.min.css", "../extension/custom.css", "../extension/font/bootstrap-icons.css"],
-  js: ["../extension/jquery.min.js", "../extension/bootstrap.bundle.min.js"]
+  js: ["../extension/jquery.min.js", "../extension/bootstrap.bundle.min.js","../extension/html2canvas.min.js"]
 };
 const cssList = config.css.map((url) => `<link rel="stylesheet" href="${url}">`).join("");
 const jsList = config.js.map((url) => `<script src="${url}"></script>`).join("");
@@ -30,3 +30,5 @@ if (window.location.pathname === '/login.html') {
 
 const ipAddress = localStorage.getItem('ipAddress');
 const port = localStorage.getItem('port');
+const url = `http://${ipAddress}:${port}`;
+console.log(url);
